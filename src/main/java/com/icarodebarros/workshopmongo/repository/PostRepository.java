8932f992e28b7d45f7e68b19dbeb7837eb927255
Icaro de Barros <icarodebarros@gmail.com>
2020-07-22 12:05:38 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/icarodebarros/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.icarodebarros.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.icarodebarros.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
