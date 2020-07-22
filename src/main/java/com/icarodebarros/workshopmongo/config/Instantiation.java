16ac8bca916e71bde37b18f213198a86477b4f2b
Icaro de Barros <icarodebarros@gmail.com>
2020-07-22 13:33:41 -0300

    Endpoint p obter um post por id

--- src/main/java/com/icarodebarros/workshopmongo/config/Instantiation.java
+++ src/main/java/com/icarodebarros/workshopmongo/config/Instantiation.java
@@ -36,7 +36,7 @@ public class Instantiation implements CommandLineRunner {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abreços!", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));		
 				
 		postRepository.saveAll(Arrays.asList(post1, post2));
