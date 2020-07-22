3e20d1aaa0ccbba7cbcf7e2db9a56e1598e41779
Icaro de Barros <icarodebarros@gmail.com>
2020-07-22 14:13:47 -0300

    Consulta simples com @Query

--- src/main/java/com/icarodebarros/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/icarodebarros/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.icarodebarros.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.icarodebarros.workshopmongo.domain.Post;
@@ -12,5 +13,14 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	// Documentação: https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#reference
 	// Buscar pela sessão "Query Methods"
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// OU
+	
+	//Documentação: https://docs.mongodb.com/manual/reference/operator/query/regex/
+	//@Query("{ <field>: { $regex: /pattern/, $options: '<options>' } }")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 -> primeito parametro; i -> ingnore case
+	List<Post> searchTitle(String text);
+	
+	
 
 }
